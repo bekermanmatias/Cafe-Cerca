cbea657dacfe0a1befb9491b3d13916e2e12f60b
Matias Rau Bekerman <bekermanmatias@gmail.com>
2025-07-27 21:29:28 -0300

    add: cafeteria correctas en las visitas

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -6,6 +6,13 @@ export default function RootLayout() {
     <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen 
+          name="add-visit" 
+          options={{ 
+            headerShown: false,
+            animation: 'slide_from_right'
+          }} 
+        />
       </Stack>
     </ThemeProvider>
   );
